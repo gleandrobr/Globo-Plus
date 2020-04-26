c30249351b0b196269dc2cb4bb843da4b32b906c
gabrieldev525 <gvictor525.gv@gmail.com>
2020-04-26 16:06:53 -0300

    add delete methods

--- front/src/store/utils.js
+++ front/src/store/utils.js
@@ -2,22 +2,34 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3333/'
 
-export const get = (url, type) => {
-  return axios.get(url)
-    .then((response) => {
-      return { type: type, payload: response }
-    })
-    .catch((error) => {
-      throw error
-    })
-}
+export const requests = {
+  get(url, type) {
+    return axios.get(url)
+      .then((response) => {
+        return { type: type, payload: response }
+      })
+      .catch((error) => {
+        throw error
+      })
+  },
 
-export const post = (url, data, type) => {
-  return axios.post(url, data)
-    .then((response) => {
-      return { type: type, payload: response }
-    })
-    .catch((error) => {
-      throw error
-    })
+  post(url, data, type) {
+    return axios.post(url, data)
+      .then((response) => {
+        return { type: type, payload: response }
+      })
+      .catch((error) => {
+        throw error
+      })
+  },
+
+  delete(url, params, type) {
+    return axios.delete(url)
+      .then((response) => {
+        return { type: type, payload: response }
+      })
+      .catch((error) => {
+        throw error
+      })
+  }
 }
