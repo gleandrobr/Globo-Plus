632c89253ab3c8dbf1d0899cdd954554e3e98b5e
garciamendes <garciaprog0101@gmail.com>
2020-05-04 02:23:49 -0300

    alters in routes

--- front/src/routes.js
+++ front/src/routes.js
@@ -19,7 +19,7 @@ const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='NewsDetails'
+        initialRouteName='Splash'
         screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name='Splash'
